99adb664a88d7400ea36d6e8d77b9dbfd2c7129b
AnaLinsDev <analinsdev@gmail.com>
2026-08-19 17:53:16 -0300

    Creating project with layered archictecture and adding nuget package for validation

---
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -0,0 +1,40 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        try
+        {
+            var reponse = new RegisterExpenseUseCase().Execute(request);
+            return Created(string.Empty, reponse);
+        }
+        catch (ArgumentException ex)
+        {
+            var errorResponse = new ResponseErrorJson
+            {
+                ErrorMessage = ex.Message
+            };
+
+            return BadRequest(errorResponse);
+        }
+        catch
+        {
+            var errorResponse = new ResponseErrorJson
+            {
+                ErrorMessage = "Unknown error"
+            };
+
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+        }
+    }
+}
+S
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,48 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+
+        validate(request);
+
+        return new ResponseRegisterExpenseJson
+        {
+            Title = request.Title,
+        };
+    }
+
+    private void validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("The title is required.");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than 0.");
+        }
+
+        var dateComparison = DateTime.Compare(request.Date, DateTime.UtcNow);
+
+        if (dateComparison > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future.");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+
+        if (!paymentTypeIsValid)
+        {
+            throw new ArgumentException("Payment Type is not valid.");
+        }
+    }
+}
+
