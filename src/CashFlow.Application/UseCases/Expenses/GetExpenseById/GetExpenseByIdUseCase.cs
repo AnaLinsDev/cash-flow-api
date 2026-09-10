35ccb556f6e26da145476427b11478df6bd1a09c
AnaLinsDev <analinsdev@gmail.com>
2026-09-10 17:53:52 -0300

    add notfound exception

--- src/CashFlow.Application/UseCases/Expenses/GetExpenseById/GetExpenseByIdUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/GetExpenseById/GetExpenseByIdUseCase.cs
@@ -2,6 +2,8 @@
 using CashFlow.Application.UseCases.Expenses.GetById;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.GetExpenseById;
 public class GetExpenseByIdUseCase : IGetExpenseByIdUseCase
@@ -19,6 +21,10 @@ public class GetExpenseByIdUseCase : IGetExpenseByIdUseCase
     public async Task<ResponseExpenseJson> Execute(long id)
     {
         var result = await _repository.GetById(id);
+
+        if (result is null) {
+            throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+        }
 
         return _mapper.Map<ResponseExpenseJson>(result);
     }
