a944cac6fb399685845749b5d3d8246b85a53f14
AnaLinsDev <analinsdev@gmail.com>
2026-09-14 17:47:44 -0300

    add the repository method to filter the expenses by year-month

--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -1,12 +1,21 @@
 ﻿using CashFlow.Domain.Reports;
+using CashFlow.Domain.Repositories.Expenses;
 using ClosedXML.Excel;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
 public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
 {
+    private readonly IExpensesReadOnlyRepository _repository;
+    public GenerateExpensesReportExcelUseCase(IExpensesReadOnlyRepository repository)
+    {
+        _repository = repository;
+    }
+
     public async Task<byte[]> Execute(DateOnly month)
     {
-       var workbook = new XLWorkbook();
+        var expenses = await _repository.FilterByMonth(month);
+
+        var workbook = new XLWorkbook();
 
         workbook.Author = "Ana Julia Lins";
         workbook.Style.Font.FontSize = 12;
