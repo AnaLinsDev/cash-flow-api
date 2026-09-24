f333545146cbebcffddbe63ee2f3b6505f66410c
AnaLinsDev <analinsdev@gmail.com>
2026-09-24 15:51:27 -0300

    add cryptography on the user password

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Security.Cryptography;
 using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -13,8 +14,13 @@ public static class DependencyInjectionExtension
     {
         AddDbContext(services, configuration);
         AddRepositories(services);
+        AddSecurity(services);
     }
 
+    private static void AddSecurity(IServiceCollection services)
+    {
+        services.AddScoped<IPasswordEncripter, Security.BCrypt>();
+    }
 
     private static void AddRepositories(IServiceCollection services)
     {
@@ -31,4 +37,6 @@ public static class DependencyInjectionExtension
 
         services.AddDbContext<CashFlowDbContext>(config => config.UseNpgsql(connectionString));
     }
+
+    
 }
